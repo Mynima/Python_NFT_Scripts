92b2e86cc2cfcfad09ebc7db87f40d72b49bf8f6
Mynima <62206239+Mynima@users.noreply.github.com>
2021-06-18 22:30:58 +0100

    Update 02_download_wallet.py

--- 02_download_wallet.py
+++ 02_download_wallet.py
@@ -21,16 +21,19 @@ if not os.path.exists(newpath):
 #Get all the assets for the wallet
 assets = atom.get_assets(owner=walletID)
 
-#Go through folder assets and download the images.
+#Go through assets and download the images to folder.
 for a in assets:
     asset = atom.get_asset(a.key)
     name = asset.name
     image = asset.image
     response = requests.get(image)
-    if asset.name.upper().find("CREATION") > 0 or asset.name.upper().find("ANIMATE") > 0:
+    if asset.name.upper().find("CREATION") > 0 or asset.name.upper().find("ANIMATE") > 0 or asset.image.upper().find(".GIF") > 0:
         ext = "gif"
     else:
         ext = "png"
+    print(name)
+    print(image)
+    print(ext)
     if name not in exclude_list:
         exclude_list.append(str(name))
         file = open(f"{newpath}/{name}.{ext}", "wb")
